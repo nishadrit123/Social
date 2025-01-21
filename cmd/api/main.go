4728b454985f18621f09ad2ed755f7bd66d275a2
Nishad <nishadkanago@gmail.com>
2025-01-21 08:47:33 +0530

    storage struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"social/internal/env"
+	"social/internal/store"
 
 	"github.com/joho/godotenv"
 )
@@ -15,8 +16,10 @@ func main() {
 	cfg := &config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
 	app := &application{
 		config: *cfg,
+		store:  store,
 	}
 	mux := app.mount()
 
